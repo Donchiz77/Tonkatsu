cf0b81f6cb589c59b4c08ec8a440aeb06d9def9c
Don Chisolm <don.chisolm14@gmail.com>
2021-05-15 12:40:22 -0400

    lines 27- 56 added needs debbuging

--- script/script.js
+++ script/script.js
@@ -16,13 +16,8 @@ var toUpper = function (x) {
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  var userLength = (prompt("How many characters? No less than 8, no more than 128"));
-  while(confirmLength <= 8 || confirmLength >= 128) {
-    alert("Password length must be between 8-128 characters Try again");
-  var userLength = (prompt("How many characters would you like your password to contain?"));
-      } 
+  
 };
-//not sure of placement debugging needed
   passwordText.value = password;
 
 }
@@ -30,5 +25,33 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function writePassword(){
+  var userLength = (prompt("How many characters? No less than 8, no more than 128"));
+    while(userLength <= 8 || userLength >= 128) {
+      alert("Password length must be between 8-128 characters Try again");
+  var userLength = (prompt("How many characters would you like your password to contain?"));
+      }
+      var userCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
+      var userlowercase = confirm("Click OK to confirm if you would like to include lowercase characters");
+      var userUppercase = confirm("Click OK to confirm if you would like to include uppercase characters");  
+      var passwordCharacters = []
+
+    if (userCharacter) {
+      passwordCharacters = passwordCharacters.concat(specialChar)
+    } if (confirmNumericCharacter) {
+      passwordCharacters = passwordCharacters.concat(number)
+    }
+
+    if (userlowercase) {
+      passwordCharacters = passwordCharacters.concat(alphaLower)
+    }
+
+    if (userUppercase) {
+      passwordCharacters = passwordCharacters.concat(alphaUpper)
+    }
   
+      for (var i = 0; i < userLength; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+      }
+      return randomPassword;
 }
