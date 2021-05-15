3224a7be584c1ba92b0656cddd48caa0442abd0f
Don Chisolm <don.chisolm14@gmail.com>
2021-05-15 02:08:28 -0400

    length variables added needs debugint lines 17-22

--- script/script.js
+++ script/script.js
@@ -1,15 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var userNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+var userNumber =["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var userCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
 var userlowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var useruppercase = [];  
+var userUppercase = []; 
+var userLength= "";
+var answers;
+var toUpper = function (x) {
+  return x.toUpperCase();
+  //usure about this part will test and adjsut later
+  
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var userLength = (prompt("How many characters? No less than 8, no more than 128"));
+  while(confirmLength <= 8 || confirmLength >= 128) {
+    alert("Password length must be between 8-128 characters Try again");
+  var userLength = (prompt("How many characters would you like your password to contain?"));
+      } 
+};
+//not sure of placement debugging needed
   passwordText.value = password;
 
 }
@@ -17,9 +30,5 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function writePassword(){
-  if (prompt("Any special characters?")) {
-    console.log("");
-  } else {
-    console.log("");
-  }
+  
 }
