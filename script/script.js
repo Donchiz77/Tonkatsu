a7c869973496f3ef783ff487b53a8c12c6220df9
Don Chisolm <don.chisolm14@gmail.com>
2021-05-15 23:10:13 -0400

    file reworked entirely still broken. cannot get randomized password to generate.

--- script/script.js
+++ script/script.js
@@ -1,55 +1,104 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var userNumber =["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var userCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-var userlowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var userUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
-var userLength= "";
+var { generateBtn, userNumber, userlowercase, userUppercase } = passvalue(); 
+
+function newFunction() {
+  var generateBtn = document.querySelector("#generate");
+  var userNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  var userCharacter = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+  var userlowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var userUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  return { generateBtn, userNumber, userlowercase, userUppercase };
+}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("password");
 
-  passwordText.getElementById("#password").value = password;
+  passwordText.value = password;
 
-}
-function resetText(){
-  document.getElementById(aria-label).value = "Your password here";
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 function writePassword(){
-  var userLength = (prompt("How many characters? No less than 8, no more than 128"));
-    while(userLength <= 7 || userLength >= 128) {
-      alert("Password length must be between 8-128 characters Try again");
-  var userLength = (prompt("How many characters would you like your password to contain?"));
-      }
-      var userCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
-      var userlowercase = confirm("Click OK to confirm if you would like to include lowercase characters");
-      var userUppercase = confirm("Click OK to confirm if you would like to include uppercase characters");  
-      var passwordText = []
-
-    if (userinputSpecial) {
-      passwordText = passwordText.concat(userCharacter)
-    } 
-    if (userNumber) {
-      passwordText = passwordText.concat(userinputNumeric)
-    }
-
-    if (userlowercase) {
-      passwordText = passwordText.concat(userlowercase)
-    }
-
-    if (userUppercase) {
-      passwordText = passwordText.concat(userUppercase)
-    }
-  
-    var randomPassword = ""
-      for (var i = 0; i < userLength; i++) {
-        randomPassword = randomPassword + passwordText[Math.floor(Math.random() * passwordText.length)];
-      }
-      return randomPassword;
+  var length = parseInt(prompt("How many characters would you like? No more than 128, no less than 8."))
+  if (length < 8) {
+    alert('The password must be at least 8 characters!');
+    return;
 }
+if (length > 128) {
+    alert('The password must be less than 128 characters!');
+    return;
+}
+
+var shouldIncludeSpecialCharacters = confirm("Do you want to include special characters?");
+
+var shouldIncludeNumeric = confirm("Do you want to include numeric characters?");
+
+var shouldIncludeLowercase = confirm("Do you want to include lowercase characters?");
+
+var shouldIncludeUppercase = confirm("Do you want to include uppercase characters?");
+
+if (!shouldIncludeLowercase && !shouldIncludeUppercase && !shouldIncludeNumeric && !shouldIncludeSpecialCharacters) {
+    alert("Your password must contain at least one special, numeric, lowercase, or uppercase character");
+    return;
+}
+
+var questionOptions = {
+    length: length,
+    specialCharacters: shouldIncludeSpecialCharacters,
+    numeric: shouldIncludeNumeric,
+    lowerCase: shouldIncludeLowercase,
+    upperCase: shouldIncludeUppercase
+}
+
+return questionOptions;
+
+}
+
+function generatePassword() {
+
+var options = generateOptions();
+console.log(options)
+
+var passwordPool = [];
+console.log(passwordPool)
+
+if (options.specialCharacters) {
+    for (i = 0; i < onlySpecial.length; ++i) {
+        passwordPool.push(onlySpecial[i]);
+    }
+} 
+if (options.numeric) {
+    for (i = 0; i < userNumber.length; ++i) {
+    passwordPool.push(userNumber[i]);
+    }
+}
+if (options.lowerCase) {
+    for (i = 0; i < userlowercase.length; ++i) {
+    passwordPool.push(userlowercase[i]);
+    }
+}
+if (options.upperCase) {
+    for (i = 0; i < userUppercase.length; ++i) {
+    passwordPool.push(userUppercase[i]);
+    }
+}
+
+var finalPassword = [];
+
+for (let i = 0; i < options.length; ++i) {
+    var randomPicker = Math.floor(Math.random() * Math.floor(passwordPool.length));
+     finalPassword.push(passwordPool[randomPicker])
+  }
+  console.log(finalPassword)
+
+  var superFinal = finalPassword.join('');
+  console.log(password)
+
+  document.getElementById("password").textContent = superFinal;
+}
+
+
